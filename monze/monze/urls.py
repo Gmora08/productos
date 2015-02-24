9b369431e2418445166a082ee3d596d3a8f0dac6
Gustavo <mnr.hh08@gmail.com>
2015-02-24 09:53:30 -0600

    editar producto

--- monze/monze/urls.py
+++ monze/monze/urls.py
@@ -6,8 +6,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'monze.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^producto', vproductos.productos, name="mostrar"),
-    url(r'^producto/agregar', vproductos.AgregarProducto.as_view(), name="agregar-producto"),
+    url(r'^producto/$', vproductos.productos, name='productos'),
+    url(r'^producto/agregar/$', vproductos.AgregarProducto.as_view(), name="agregar-producto"),
+    url(r'^producto/editar/(?P<id_producto>\d{1,})/$', vproductos.EditarProducto.as_view(), name="editar-producto"),
+
 )
--- monze/producto/views.py
+++ monze/producto/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.views.generic.base import View
 from django.contrib import messages
 from .forms import ProductoForm
 from .models import Producto
 
 def productos(request):
+    print "hola"
     productos = Producto.objects.all()
     return render(request, 'productos.html', {'productos': productos})
+
+class EditarProducto(View):
+    def get(self, request, id_producto):
+        print "ya entre"
+        buscar = get_object_or_404(Producto, pk=id_producto)
+        form = ProductoForm(instance=buscar)
+        return render(request, 'editar-producto.html', {'form': form})
+
+    def post(self, request, id_producto):
+        buscar = get_object_or_404(Producto, pk=id_producto)
+        form = ProductoForm(request.POST, instance=buscar)
+        if form.is_valid():
+            form.save()
+            messages.success(request, u"Producto %s editado con exito " % request.POST['nombre'])
+            return redirect(reverse('productos'))
+        else:
+            messages.error(request, u"ingresa mal algunos datos")
+            return render(request, 'editar-producto.html', {'form':form})
+
 
 class AgregarProducto(View):
     def get(self, request):     
@@ -17,6 +38,8 @@ class AgregarProducto(View):
         if form.is_valid():
             form.save()
             messages.success(request, u"Producto %s creado con exito " % request.POST['nombre'])
+            return redirect(reverse('productos'))
+            
 
         else:
             messages.error(request, u"ingresa mal algunos datos")
